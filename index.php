c563944e7f6b683b7eb2015fdd9800ba40ce5b25
felipe-alves142 <77590921+felipe-alves142@users.noreply.github.com>
2021-06-28 20:24:35 -0300

    Update index.php

--- index.php
+++ index.php
@@ -107,7 +107,7 @@
             <p>É um software de avaliação entre colegas de trabalho atraves de elogios. <br>
               Nele o superior pode avaliar o trabalho entrege pelo desenvolvedor. 
             </p>    
-            <a href="https://github.com/felipe-alves142/nlwValoriza">Veja no Github</a>
+            <a href="https://github.com/felipe-alves142/nlwValoriza">Veja no Github</a> <br>
             <a href="https://felipe-alves142.github.io//the_perfector/paginas/index.html">Veja uma demo</a>
         </div>
         <div>
